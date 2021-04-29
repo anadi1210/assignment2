3eadc120ca2adf64e595f64a04ea0101859b4329
SidneyTWilliams <82415751+SidneyTWilliams@users.noreply.github.com>
2021-04-28 21:09:58 -0500

    Update CDAccount.java

--- src/main/java/com/meritamerica/assignment2/CDAccount.java
+++ src/main/java/com/meritamerica/assignment2/CDAccount.java
@@ -4,4 +4,48 @@ import java.util.Date;
 
 public class CDAccount {
 
+	private double balance;
+	private double interestRate;
+	private int term;
+	private long accountNumber;
+	
+	CDAccount(CDOffering offering, double balance) {
+		
+	}
+	
+	
+	
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+
+
+	public void setInterestRate(double interestRate) {
+		this.interestRate = interestRate;
+	}
+
+
+	public void setTerm(int term) {
+		this.term = term;
+	}
+
+
+	public void setAccountNumber(long accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+
+
+	
+	
+	java.util.Date getStartDate() {
+		
+		java.util.Date currentDate = new java.util.Date();
+		return currentDate;
+	}
+	
+	double futureValue() {
+		
+		return 0.0;
+	}
+
 }
