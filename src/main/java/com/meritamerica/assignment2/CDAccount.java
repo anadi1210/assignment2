de064a627d548f5dddbaaeae6ab09ce73b30cec7
Anadi Sharma <82360415+anadi1210@users.noreply.github.com>
2021-05-05 15:15:41 -0500

    Update CDAccount.java

--- src/main/java/com/meritamerica/assignment2/CDAccount.java
+++ src/main/java/com/meritamerica/assignment2/CDAccount.java
@@ -2,50 +2,72 @@ package com.meritamerica.assignment2;
 
 import java.util.Date;
 
-public class CDAccount {
+public class CDAccount extends BankAccount{
 
 	private double balance;
 	private double interestRate;
 	private int term;
 	private long accountNumber;
-	
-	CDAccount(CDOffering offering, double balance) {
-		
+	CDOffering offering ;
+
+	CDAccount(CDOffering offering, double balance){
+		this.offering = offering;
+		this.term = offering.getTerm();
+		this.interestRate = offering.getInterestRate();
+		this.balance = balance;
 	}
 	
 	
 	
+	public double getBalance() {
+		return balance;
+	}
+
 	public void setBalance(double balance) {
 		this.balance = balance;
 	}
 
+	public double getInterestRate() {
+		return interestRate;
+	}
 
 	public void setInterestRate(double interestRate) {
 		this.interestRate = interestRate;
 	}
 
+	public int getTerm() {
+		return term;
+	}
 
 	public void setTerm(int term) {
 		this.term = term;
 	}
 
+	public long getAccountNumber() {
+		return accountNumber;
+	}
 
 	public void setAccountNumber(long accountNumber) {
 		this.accountNumber = accountNumber;
 	}
 
+	public CDOffering getOffering() {
+		return offering;
+	}
 
-	
-	
-	java.util.Date getStartDate() {
+	public void setOffering(CDOffering offering) {
+		this.offering = offering;
+	}
+
+	Date getStartDate(){
 		
-		java.util.Date currentDate = new java.util.Date();
-		return currentDate;
+		return null;
 	}
 	
 	double futureValue() {
 		
 		return 0.0;
 	}
+
 
 }
