9ad25fc33576d7985fb331f7ac7096de14c3add8
Anadi Sharma <82360415+anadi1210@users.noreply.github.com>
2021-05-05 15:18:47 -0500

    Update SavingsAccount.java

--- src/main/java/com/meritamerica/assignment2/SavingsAccount.java
+++ src/main/java/com/meritamerica/assignment2/SavingsAccount.java
@@ -1,6 +1,42 @@
 package com.meritamerica.assignment2;
 
-public class SavingsAccount {
+public class SavingsAccount extends BankAccount{
 
+	
+	private double balance;
+	
+	
+	public SavingsAccount() {
+	
+	}
+	 SavingsAccount(double openingBalance){
+		 this.balance = openingBalance;
+	 }
+	
+	public double getBalance() {
+		return balance;
+	}
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
 
+	boolean withdraw(double amount) {
+		boolean flag = false;
+		if(amount != 0 && amount <= balance) {
+			balance -= amount;
+			flag = true;
+		}
+		
+		return flag;
+	}
+	// This method  will receive the amount to be deposited and will return boolean value as output.
+	boolean deposit(double amount) {
+		boolean flag = false;
+		if(amount >= 0.0) {
+			balance += amount;
+			flag = true;
+		}
+		return flag;
+	}
+	
 }
